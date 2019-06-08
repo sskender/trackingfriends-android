d9ff1dc5b80f641b69dcf752ac0a3579e1913bca
sskender <sskender@users.noreply.github.com>
2019-06-08 15:16:46 +0200

    RestInterface location implemented

--- app/src/main/java/com/example/friendslocation/net/retrofit/RestRetrofit.kt
+++ app/src/main/java/com/example/friendslocation/net/retrofit/RestRetrofit.kt
@@ -1,5 +1,6 @@
 package com.example.friendslocation.net.retrofit
 
+import com.example.friendslocation.entity.Location
 import com.example.friendslocation.entity.User
 import com.example.friendslocation.entity.UserPublicProfile
 import com.example.friendslocation.net.RestFactory
@@ -11,6 +12,7 @@ import retrofit2.Retrofit
 class RestRetrofit : RestInterface {
 
     private val userService: UserService
+    private val locationService: LocationService
 
     init {
         val SERVER_URL = "http://" + RestFactory.SERVER_IP + ":" + RestFactory.SERVER_PORT + "/api/v1/"
@@ -20,6 +22,7 @@ class RestRetrofit : RestInterface {
             .build()
 
         this.userService = retrofit.create(UserService::class.java)
+        this.locationService = retrofit.create(LocationService::class.java)
     }
 
     override fun registerUser(user: User): UserPublicProfile? {
@@ -58,6 +61,28 @@ class RestRetrofit : RestInterface {
     override fun updateUserProfile(user: User): UserPublicProfile? {
         val call: Call<UserPublicProfile> = userService.updateProfile(user.userId, user)
         val response: Response<UserPublicProfile> = call.execute()
+
+        if (response.isSuccessful) {
+            return response.body()
+        } else {
+            TODO("not implemented")
+        }
+    }
+
+    override fun updateUserLocation(location: Location): Location? {
+        val call: Call<Location> = locationService.updateLocation(location.userId, location)
+        val response: Response<Location> = call.execute()
+
+        if (response.isSuccessful) {
+            return response.body()
+        } else {
+            TODO("not implemented")
+        }
+    }
+
+    override fun getFriendsLocation(userId: String, friendId: String): Location? {
+        val call: Call<Location> = locationService.friendLocation(userId, friendId)
+        val response: Response<Location> = call.execute()
 
         if (response.isSuccessful) {
             return response.body()
