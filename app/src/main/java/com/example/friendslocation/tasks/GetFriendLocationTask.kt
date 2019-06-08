60264d80c0e4ad68e8f8912625eb029924fe7c59
sskender <sskender@users.noreply.github.com>
2019-06-09 00:25:18 +0200

    Async tasks preview

---
+++ app/src/main/java/com/example/friendslocation/tasks/GetFriendLocationTask.kt
@@ -0,0 +1,14 @@
+package com.example.friendslocation.tasks
+
+import android.os.AsyncTask
+import com.example.friendslocation.entity.Location
+import com.example.friendslocation.net.RestFactory
+
+class GetFriendLocationTask : AsyncTask<Pair<String, String>, Unit, Location>() {
+
+    override fun doInBackground(vararg params: Pair<String, String>): Location? {
+        val rest = RestFactory.instance
+        return rest.getFriendsLocation(params[0].first, params[0].second)
+    }
+
+}
---
+++ app/src/main/java/com/example/friendslocation/tasks/LoginUserTask.kt
@@ -0,0 +1,15 @@
+package com.example.friendslocation.tasks
+
+import android.os.AsyncTask
+import com.example.friendslocation.entity.User
+import com.example.friendslocation.entity.UserPublicProfile
+import com.example.friendslocation.net.RestFactory
+
+class LoginUserTask : AsyncTask<User, Unit, UserPublicProfile?>() {
+
+    override fun doInBackground(vararg params: User): UserPublicProfile? {
+        val rest = RestFactory.instance
+        return rest.loginUser(params[0])
+    }
+
+}
