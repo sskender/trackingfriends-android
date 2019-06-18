eb3fa67ebc2fb439fc9bec32b52717e0ae8c7bb6
sskender <sskender@users.noreply.github.com>
2019-06-18 20:24:16 +0200

    Async task to update location created

--- app/src/main/java/com/example/friendslocation/tasks/UpdateUserLocationTask.kt
+++ app/src/main/java/com/example/friendslocation/tasks/UpdateUserLocationTask.kt
@@ -1,14 +1,40 @@
 package com.example.friendslocation.tasks
 
 import android.os.AsyncTask
+import android.util.Log
 import com.example.friendslocation.entity.Location
 import com.example.friendslocation.net.RestFactory
 
-class UpdateUserLocationTask : AsyncTask<Location, Unit, Unit>() {
+class UpdateUserLocationTask : AsyncTask<String, Unit, Unit>() {
 
-    override fun doInBackground(vararg params: Location) {
+    private fun grabCurrentGPSLocation(): Location {
+
+        // TODO grab from gps
+
+        var latitude = (10..100).random().toDouble()
+        var longitude = (10..100).random().toDouble()
+
+        Log.d("MY_LOCATION", "lat: $latitude, lon: $longitude")
+
+        return Location("", latitude, longitude)
+    }
+
+    override fun doInBackground(vararg params: String) {
         val rest = RestFactory.instance
-        rest.updateUserLocation(params[0])
+
+        while (true) {
+
+            // craft location
+            val location: Location = grabCurrentGPSLocation()
+            location.userId = params[0]
+
+            // send to server
+            rest.updateUserLocation(location)
+
+            // sleep some time
+            Thread.sleep(2000)
+        }
+
     }
 
 }
