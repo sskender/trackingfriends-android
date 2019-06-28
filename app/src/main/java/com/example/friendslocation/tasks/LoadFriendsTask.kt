d1fefe714aa4b31f51ff557ca57b3d51ecf8adb5
sskender <sskender@users.noreply.github.com>
2019-06-28 03:35:27 +0200

    Load friends task and adapter

---
+++ app/src/main/java/com/example/friendslocation/tasks/LoadFriendsTask.kt
@@ -0,0 +1,32 @@
+package com.example.friendslocation.tasks
+
+import android.os.AsyncTask
+import com.example.friendslocation.adapter.FriendsAdapter
+import com.example.friendslocation.dao.UserPublicProfileList
+import com.example.friendslocation.entity.UserPublicProfile
+import com.example.friendslocation.net.RestFactory
+
+class LoadFriendsTask(
+    private val userId: String,
+    private val friendsAdapter: FriendsAdapter
+) :
+    AsyncTask<Unit, Unit, List<UserPublicProfile>?>() {
+
+    override fun doInBackground(vararg p0: Unit?): List<UserPublicProfile>? {
+        val rest = RestFactory.instance
+
+        return rest.getUserFriends(userId)
+    }
+
+    override fun onPostExecute(result: List<UserPublicProfile>?) {
+
+        if (result != null) {
+            UserPublicProfileList.list = result as MutableList<UserPublicProfile>
+        }
+
+        // update adapter
+        friendsAdapter.notifyDataSetChanged()
+
+        super.onPostExecute(result)
+    }
+}
